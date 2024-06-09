3094821290a533fd750a56b1c0f25151e4382526
AJ Ruth Sumandang <aj_decode@edamama.ph>
2024-06-10 01:24:15 +0800

    chore: update cors policy

--- apps/Api/Startup.cs
+++ apps/Api/Startup.cs
@@ -1,30 +1,61 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace ItemCatalogue.Api
 {
-    public class Startup
+  public class Startup
+  {
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+
+      var allowedOrigins = Configuration.GetSection("AllowedOrigins").Get<string[]>();
+
+      services.AddCors(options =>
+        {
+          options.AddPolicy("AllowSpecificOrigins",
+              builder =>
+              {
+                builder.WithOrigins(allowedOrigins)
+                         .AllowAnyHeader()
+                         .AllowAnyMethod();
+              });
+        });
+      if (allowedOrigins != null)
+      {
+        foreach (var origin in allowedOrigins)
+        {
+          Console.WriteLine(origin);
+        }
+      }
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseRouting();
+
+      app.UseCors("AllowSpecificOrigins");
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
