d5131309036ea847ef137b62b28f385173a57332
AJ Ruth Sumandang <aj_decode@edamama.ph>
2024-06-09 21:03:42 +0800

    feat: add top-nav secondary endpoint for home ui

---
+++ libs/home/ui/top-nav/README.md
@@ -0,0 +1,3 @@
+# @item-catalogue/source/top-nav
+
+Secondary entry point of `@item-catalogue/source`. It can be used by importing from `@item-catalogue/source/top-nav`.
---
+++ libs/home/ui/top-nav/src/top-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-top-nav',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './top-nav.component.html',
+  styleUrl: './top-nav.component.css',
+})
+export class TopNavComponent {}
