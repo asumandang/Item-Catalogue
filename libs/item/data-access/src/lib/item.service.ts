cf9a1e4681845874aa3b04ec1d6db486b88e0238
AJ Ruth Sumandang <aj_decode@edamama.ph>
2024-06-10 01:41:26 +0800

    feat: add dto library and update shapes in the data access

--- libs/item/data-access/src/lib/item.service.ts
+++ libs/item/data-access/src/lib/item.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { API_V1_PREFIX } from '@item-catalogue/http-core';
+import { Item } from '@item-catalogue/dto';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,6 @@ export class ItemService {
   apiV1Prefix = inject(API_V1_PREFIX);
 
   getItems() {
-    return this.httpClient.get(`${this.apiV1Prefix}/items`);
+    return this.httpClient.get<Item[]>(`${this.apiV1Prefix}/items`);
   }
 }
--- libs/item/feature/item-list/src/lib/item-list.component.ts
+++ libs/item/feature/item-list/src/lib/item-list.component.ts
@@ -1,14 +1,16 @@
-import { ItemService } from './../../../../data-access/src/lib/item.service';
+import { ItemService } from '@item-catalogue/item-data-access';
 import { Component, inject } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { AsyncPipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'item-catalogue-item-list',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, NgFor, AsyncPipe],
   templateUrl: './item-list.component.html',
   styleUrl: './item-list.component.css',
 })
 export class ItemListComponent {
   itemService = inject(ItemService);
+  item$ = this.itemService.getItems();
 }
