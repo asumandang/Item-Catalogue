ac1d8ddbfae97bc8675fe3d1c61150f79237079c
AJ Ruth Sumandang <aj_decode@edamama.ph>
2024-06-09 20:49:47 +0800

    refactor: rearrange home ui library

--- libs/home/ui/README.md
+++ libs/home/ui/README.md
@@ -1,7 +1,7 @@
-# ui
+# home-ui
 
 This library was generated with [Nx](https://nx.dev).
 
 ## Running unit tests
 
-Run `nx test ui` to execute the unit tests.
+Run `nx test home-ui` to execute the unit tests.
--- libs/home/ui/jest.config.ts
+++ libs/home/ui/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'ui',
-  preset: '../../../../jest.preset.js',
+  displayName: 'home-ui',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../../../coverage/libs/home/ui/top-nav',
+  coverageDirectory: '../../../coverage/libs/home/ui',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
