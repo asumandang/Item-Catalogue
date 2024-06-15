7916682dfd38cae1afc192d343e8b33fd575092c
AJ Ruth Sumandang <aj_decode@edamama.ph>
2024-06-15 21:40:51 +0800

    feat: inject jwt to header

--- libs/shared/interceptor/src/lib/auth.interceptor.ts
+++ libs/shared/interceptor/src/lib/auth.interceptor.ts
@@ -1,36 +1,27 @@
 import type { HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
-import {
-  catchError,
-  distinctUntilChanged,
-  startWith,
-  switchMap,
-  take,
-  tap,
-} from 'rxjs';
-import { ADMIN_AUTH_TOKEN, CONFIG } from '@item-catalogue/http-core';
+import { switchMap } from 'rxjs';
+import { ADMIN_AUTH_TOKEN } from '@item-catalogue/http-core';
+import { Store } from '@ngrx/store';
+import { userFeature } from '@item-catalogue/auth-state';
 
 export const authInterceptor = (
   req: HttpRequest<unknown>,
   next: HttpHandlerFn
 ) => {
-  if (!req.context.has(ADMIN_AUTH_TOKEN)) {
+  if (!req.context.get(ADMIN_AUTH_TOKEN)) {
     return next(req);
   }
 
-  const config = inject(CONFIG);
-  if (!config) {
-    return next(req);
-  }
-  return config.pipe(
-    take(1),
-    switchMap((config) => {
-      req = req.clone({
-        setHeaders: {
-          'X-API-Key': config.apiKey,
-        },
-      });
-      return next(req);
+  const store = inject(Store);
+  return store.select(userFeature.selectAccessToken).pipe(
+    switchMap((accessToken) => {
+      const newRequest = accessToken
+        ? req.clone({
+            headers: req.headers.set('Authorization', `Bearer ${accessToken}`),
+          })
+        : req;
+      return next(newRequest);
     })
   );
 };
