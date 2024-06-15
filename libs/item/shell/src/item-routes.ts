6a4cdef44b3c5841a9a4730f501ef789efb94eae
AJ Ruth Sumandang <aj_decode@edamama.ph>
2024-06-15 21:14:41 +0800

    feat: add guard for some pages

--- libs/item/shell/src/item-routes.ts
+++ libs/item/shell/src/item-routes.ts
@@ -1,5 +1,10 @@
-import { Route } from '@angular/router';
+import { inject } from '@angular/core';
+import { userFeature } from './../../../auth/state/src/lib/feature';
+import { Route, Router } from '@angular/router';
 import { ItemListComponent } from '@item-catalogue/item-feature-list';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export const itemRoutes: Route[] = [
   {
@@ -8,6 +13,23 @@ export const itemRoutes: Route[] = [
       { path: '', pathMatch: 'full', component: ItemListComponent },
       {
         path: 'create',
+        canActivate: [
+          () => {
+            const store = inject(Store);
+            const router = inject(Router);
+            const snackbar = inject(MatSnackBar);
+            return store.select(userFeature.selectUser).pipe(
+              map((user) => {
+                if (user) {
+                  return true;
+                }
+
+                snackbar.open('You are not authorized to open this page');
+                return router.parseUrl('/items');
+              })
+            );
+          },
+        ],
         loadComponent: () =>
           import('@item-catalogue/item-feature-create').then(
             (m) => m.ItemCreateComponent
