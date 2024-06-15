2546847b4640f9ae4a2cddd0269aed3c03183ff7
AJ Ruth Sumandang <aj_decode@edamama.ph>
2024-06-15 20:39:53 +0800

    fix: add logic to reload item view on item deletion

--- libs/item/feature/item-list/src/lib/item-list.component.ts
+++ libs/item/feature/item-list/src/lib/item-list.component.ts
@@ -3,10 +3,19 @@ import { ItemService } from '@item-catalogue/item-data-access';
 import { Component, inject } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { AsyncPipe, NgFor } from '@angular/common';
-import { exhaustMap, filter, map, switchMap } from 'rxjs';
+import {
+  exhaustMap,
+  filter,
+  map,
+  merge,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { RxIf } from '@rx-angular/template/if';
 import { rxActions } from '@rx-angular/state/actions';
 import { ConfirmationModalService } from '@item-catalogue/shared-service';
+import { rxState } from '@rx-angular/state';
+import { Item } from '@item-catalogue/dto';
 
 @Component({
   selector: 'item-catalogue-item-list',
@@ -18,22 +27,52 @@ import { ConfirmationModalService } from '@item-catalogue/shared-service';
 export class ItemListComponent {
   itemService = inject(ItemService);
   modalService = inject(ConfirmationModalService);
-  item$ = this.itemService.getItems();
+
+  private actions = rxActions<{
+    deleteItem: { itemId: string };
+    getItems: void;
+    setItems: Item[];
+  }>();
+
+  private state = rxState<{ items: Item[] }>(({ set, connect }) => {
+    set({ items: [] });
+    connect(
+      'items',
+      merge(this.itemService.getItems(), this.actions.setItems$),
+      (_state, items) => items
+    );
+  });
+  item$ = this.state.select('items');
   hasItem$ = this.item$.pipe(map((items) => items.length > 0));
 
-  private actions = rxActions<{ deleteItem: { itemId: string } }>();
   private deleteItemEffect = this.actions.onDeleteItem((deleteItem$) =>
     deleteItem$.pipe(
-      exhaustMap(() =>
+      exhaustMap(({ itemId }) =>
         this.modalService
           .open({
             title: 'Item Deletion',
             message: 'Are you sure you want to delete this item?',
             actionButton: 'Delete',
           })
-          .pipe(filter((result) => result))
-      ),
-      exhaustMap(({ itemId }) => this.itemService.deleteItem(itemId))
+          .pipe(
+            filter((result) => result),
+            switchMap(() =>
+              this.itemService
+                .deleteItem(itemId)
+                .pipe(tap(() => this.actions.getItems()))
+            )
+          )
+      )
+    )
+  );
+
+  private getItemEffect = this.actions.onGetItems((getItem$) =>
+    getItem$.pipe(
+      switchMap(() =>
+        this.itemService
+          .getItems()
+          .pipe(tap((items) => this.actions.setItems(items)))
+      )
     )
   );
 
